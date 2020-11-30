4131016e6d6a0e1da3287274c8b8265ab972875e
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-11-30 21:33:07 +0700

    Initial Commit

---
+++ GojekContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GojekContactApp
+//
+//  Created by Arifin Firdaus on 30/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
